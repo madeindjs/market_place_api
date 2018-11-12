b9af77fc088cbc553901bdbeda8f9a43e05f79de
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2018-11-12 16:12:32 +0100

    Set the routes contraints for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,6 @@
 Rails.application.routes.draw do
+  # Api definition
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/'  do
+    # We are going to list our resources here
+  end
 end
