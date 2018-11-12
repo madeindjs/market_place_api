124873774b578af3df21136df5ee80f4d50da3bd
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2018-11-12 17:36:47 +0100

    Finish chapter 2

--- Gemfile
+++ Gemfile
@@ -34,6 +34,12 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
+group :test do
+  gem 'rspec-rails', '~> 3.8'
+  gem 'factory_girl_rails', '~> 4.9'
+  gem 'ffaker', '~> 2.10'
+end
+
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- config/application.rb
+++ config/application.rb
@@ -22,10 +22,17 @@ module MarketPlaceApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.view_specs false
+      g.helper_specs false
+      g.stylesheets = false
+      g.javascripts = false
+      g.helper = false
+    end
+
+    config.autoload_paths += %W(\#{config.root}/lib)
 
     # Don't generate system test files.
     config.generators.system_tests = nil
