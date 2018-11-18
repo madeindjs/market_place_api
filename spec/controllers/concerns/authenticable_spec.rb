f729e38ae0eeadd30b292aa0998806fc8c51762d
Votre Nom <Vous@exemple.com>
2018-11-18 11:22:16 +0100

    add authenticate_with_token

--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -5,19 +5,34 @@ class Authentication < ActionController::Base
   include Authenticable
 end
 
-
 RSpec.describe Authenticable do
   let(:authentication) { Authentication.new }
   subject { authentication }
 
-  describe "#current_user" do
+  describe '#current_user' do
     before do
       @user = FactoryBot.create :user
-      request.headers["Authorization"] = @user.auth_token
+      request.headers['Authorization'] = @user.auth_token
       authentication.stub(:request).and_return(request)
     end
-    it "returns the user from the authorization header" do
+    it 'returns the user from the authorization header' do
       expect(authentication.current_user.auth_token).to eql @user.auth_token
     end
+  end
+
+  describe '#authenticate_with_token' do
+    before do
+      @user = FactoryBot.create :user
+      authentication.stub(:current_user).and_return(nil)
+      response.stub(:response_code).and_return(401)
+      response.stub(:body).and_return({ 'errors' => 'Not authenticated' }.to_json)
+      authentication.stub(:response).and_return(response)
+    end
+
+    it 'render a json error message' do
+      expect(json_response[:errors]).to eql 'Not authenticated'
+    end
+
+    it { should respond_with 401 }
   end
 end
