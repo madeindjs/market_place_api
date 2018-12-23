47ea16903968462607b580f0033579d6832307f2
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2018-12-24 00:53:52 +0100

    Adds the show action for order

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -5,4 +5,8 @@ class Api::V1::OrdersController < ApplicationController
   def index
     render json: current_user.orders
   end
+
+  def show
+    render json: current_user.orders.find(params[:id])
+  end
 end
--- spec/controllers/api/v1/orders_controller_spec.rb
+++ spec/controllers/api/v1/orders_controller_spec.rb
@@ -16,4 +16,19 @@ RSpec.describe Api::V1::OrdersController, type: :controller do
 
     it { expect(response.response_code).to eq(200) }
   end
+
+  describe 'GET #show' do
+    before(:each) do
+      current_user = FactoryBot.create :user
+      api_authorization_header current_user.auth_token
+      @order = FactoryBot.create :order, user: current_user
+      get :show, params: { user_id: current_user.id, id: @order.id }
+    end
+
+    it 'returns the user order record matching the id' do
+      expect(json_response[:id]).to eql @order.id
+    end
+
+    it { expect(response.response_code).to eq(200) }
+  end
 end
