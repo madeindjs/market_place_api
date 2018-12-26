9476f87801dd30c77a7f5844920242f374dd0cf0
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2018-12-26 17:59:45 +0100

    remove unused test

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -9,8 +9,6 @@ RSpec.describe Order, type: :model do
   it { should respond_to(:user_id) }
 
   it { should validate_presence_of :user_id }
-  it { should validate_presence_of :total }
-  it { should validate_numericality_of(:total).is_greater_than_or_equal_to(0) }
 
   it { should belong_to :user }
 
