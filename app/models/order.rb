0b754599204ee0798c932724c32440cb1d4a5209
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2018-12-28 11:31:23 +0100

    Adds validator for order with not enough products on stock

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,6 +5,7 @@ class Order < ApplicationRecord
   has_many :products, through: :placements
   validates :total, numericality: { greater_than_or_equal_to: 0.0 }, presence: true
   validates :user_id, presence: true
+  validates_with EnoughProductsValidator
 
   before_validation :set_total!
 
